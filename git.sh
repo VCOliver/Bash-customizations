93915eaf4a328faf4199c068289db6cd10998080
VCOliver <victorcruz.01@gmail.com>
2025-02-06 23:26:29 -0300

    Accidentally printing an extra ':'

--- git.sh
+++ git.sh
@@ -9,4 +9,4 @@ GIT_COLOR=$BOLD_CYAN
 BRANCH_COLOR=$LIGHT_YELLOW
 
 # Customize the prompt; %s is replaced with the branch name by __git_ps1.
-export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(__git_ps1 \" \[${GIT_COLOR}\]git:(\[${BRANCH_COLOR}\]%s:\[${GIT_COLOR}\])\[${COLOR_RESET}\] \")\$ "
+export PS1="${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\$(__git_ps1 \" \[${GIT_COLOR}\]git:(\[${BRANCH_COLOR}\]%s\[${GIT_COLOR}\])\[${COLOR_RESET}\] \")\$ "
