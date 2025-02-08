e611e1c07a24d7c9d47d91a6f74fc13475c2a322
VCOliver <victorcruz.01@gmail.com>
2025-02-08 18:31:11 -0300

    Now git symbol only shows up when in a repo

--- git.sh
+++ git.sh
@@ -8,7 +8,7 @@ fi
 GIT_COLOR=$BOLD_CYAN
 BRANCH_COLOR=$LIGHT_YELLOW
 
-GIT_BRANCH="\$(__git_ps1 \"\[${GIT_COLOR}\]:(\[${BRANCH_COLOR}\]%s\[${GIT_COLOR}\])\[${COLOR_RESET}\]\")"
+GIT_BRANCH="\$(__git_ps1 \" \[${GIT_COLOR}\]:(\[${BRANCH_COLOR}\]%s\[${GIT_COLOR}\])\[${COLOR_RESET}\]\")"
 
 # Customize the prompt; %s is replaced with the branch name by __git_ps1.
 export GIT_BRANCH
--- prompt.sh
+++ prompt.sh
@@ -8,9 +8,8 @@ set_prompt(){
     # Build the prompt string in parts
     PS1="\[${FULL_PATH_COLOR}\][\w]\[${COLOR_RESET}\]\n"
     PS1+="${debian_chroot:+($debian_chroot)}"
-    PS1+=" \[${USER_COLOR}\]\u \[${COLOR_RESET}\]@"
+    PS1+=" \[${USER_COLOR}\]\u \[${COLOR_RESET}\]@"
     PS1+="\[${CWD_COLOR}\]\W\[${COLOR_RESET}\]"
-    PS1+="  "
     PS1+=$GIT_BRANCH
     PS1+=" -> "
 
